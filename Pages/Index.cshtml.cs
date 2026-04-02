1a11b21eae118d083222f95a61a67d8e329d9686
TakahashiNg <83152264+TakahashiNguyen@users.noreply.github.com>
2026-04-02 10:42:45 +0700

    init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TH_502045_1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
